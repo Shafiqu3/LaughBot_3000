0e30c58a5b22cf5dacac93b04db8137ddaed29f1
shafiqu3 <shafiquentale@gmail.com>
2024-10-29 00:26:22 +0300

    Insert joke here about committing to something.

---
+++ jokes.py
@@ -0,0 +1,30 @@
+print('Why don\'t scientists trust atoms?')
+input()
+print('Because they make up everything!')
+print()
+
+print('Why did the scarecrow win an award?')
+input()
+print('Because he was outstanding in his field!')
+print()
+
+print('Why don\'t some couples go to the gym?')
+input()
+print('Because some relationships don\'t work out.')
+print()
+
+print('Why did the math book look sad?')
+input()
+print('Because it had too many problems.')
+print()
+
+print('Why couldn’t the bicycle stand up by itself?')
+input()
+print('It was two-tired!')
+print()
+
+print('Why can\'t you give Elsa a balloon?')
+input()
+print('Because she will let it go!')
+print()
+
